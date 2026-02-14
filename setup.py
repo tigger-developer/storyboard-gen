58a83dcb596fce057fc419d4f9f99bbc41681228
tigger <tigger04@gmail.com>
2026-02-14 10:31:46 +0000

    release: v0.23.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="storyboard-gen",
-    version="0.23.0",
+    version="0.23.1",
     description="Generate video stills and clips from a YAML storyboard using AI image/video APIs",
     author="Taḋg Paul O'Brien",
     license="MIT",
